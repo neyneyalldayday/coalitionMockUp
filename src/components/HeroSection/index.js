f374dc0f9e7eb1542c5a966b4e3847cd424bd038
neyneyalldayday <drfrankenstein123@gmail.com>
2021-11-03 16:32:11 -0500

    hero page started not quite perfect yet

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import img from '../../images/sky.jpg'
+import img2 from '../../images/toplayer.png'
+import styled from 'styled-components'
+
+
+const HeroContainer = styled.div` 
+ background-image: url(${img});    
+ z-index: 1; 
+ background-color: bisque;
+  display: flex;  
+  justify-content: center;
+  align-items: center;
+  height: 100vh;  
+  padding: 0 1rem;
+  position: relative;
+  margin-top: -80px;
+  color: #fff;
+  `;
+
+const HeroBg = styled.div`
+  z-index: 3; 
+  background-image: url(${img2}); 
+  position: absolute;
+  background-repeat: no-repeat;
+  background-position: center;
+  max-height: 100vh;
+ 
+  object-fit: contain;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  `;
+
+
+
+
+
+const HeroContent = styled.div` 
+
+display: flex;
+flex-direction: column;
+justify-content: center;  
+height: calc(100vh - 80px);
+max-height: 100%;
+padding: 0rem calc((100vw - 1300px) /2);
+align-items: center;
+z-index: 2;
+`;
+
+const H1 = styled.h1`
+margin: 0;
+font-size: 8rem;
+position: relative;
+`;
+
+const H2 = styled.h1`
+margin: 0;
+font-size: 8rem;
+position: relative;
+color: blue;
+`;
+
+
+
+const HeroSection = () => {
+    return (
+        <HeroContainer>
+            <HeroBg/>           
+            <HeroContent>
+               <H1>LosAngles</H1>
+               <H2>Mountains</H2>
+            </HeroContent>
+            
+            
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Navbar from './Navbar'
+import "./layout.css"
+
+const Layout = ({children}) => {
+    return (
+        <>
+        <Navbar />
+        <main>{children}</main>
+            
+        </>
+    )
+}
+
+export default Layout
